747ed7884d93a770120d15e4b1f1e1f66f7f51b7
SalvatoreAlberti <alberti_salvatore@outlook.it>
2024-12-06 15:18:01 +0100

    Add task to change the color of the wall

---
+++ Mevaterse_Classroom_2/Assets/Scripts/ChangeColor.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour
+{
+    private Renderer objectRenderer;
+    private Color originalColor;
+    private bool isOriginalColor = true;
+
+    void Start()
+    {
+        objectRenderer = GetComponent<Renderer>();
+        originalColor = objectRenderer.material.color;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            if (isOriginalColor)
+            {
+                objectRenderer.material.color = new Color(
+                    Random.value, Random.value, Random.value);
+            }
+            else
+            {
+                objectRenderer.material.color = originalColor;
+            }
+            isOriginalColor = !isOriginalColor;
+        }
+    }
+}
